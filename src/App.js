e789ad3017bef16b123bb85a99f360349e690b29
Diego Alvarez <owl@Diegos-MacBook-Pro.local>
2022-06-19 11:07:57 -0300

    hire us CTA

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import amyrisSvg from './Amyris.svg'
 import tegra from './tegra.png'
 
 function App() {
-  const path = React.useRef(window.location.pathname);
   const [slide, setSlide] = React.useState(1);
   
   React.useEffect(() => {
@@ -23,20 +22,20 @@ function App() {
     <div className="App">
       <main className="App-main">
         <header>
-        { (slide === 1) ? <div className='hovered'>Root</div> : <div>Base</div>}
+        { (slide === 1) ? <div className='hovered'>Root</div> : <div>Root</div>}
         { (slide === 2) ? <div className='hovered'>What we do</div> : <div>What we do</div>}
         { (slide === 3) ? <div className='hovered'>Who we are</div> : <div>Who we are</div>}
         { (slide === 4) ? <div className='hovered'>Trusted Clients</div> : <div>Trusted Clients</div>}
           
         </header>
-        { (slide === 1) ? <ShowKubik /> : <ShowKubik hide={0}/>}
+        { (slide === 1) ? <ShowKubik /> : <ShowKubik hide={0.2}/>}
 
         <section>
           <div className='slide1'><h1 className="App-title">Catalyst</h1><p className='App-subtitle'>Unleash the speed.</p></div>
           <div className='slide2'>
       
           <div>RAIL model performance</div>
-          <div>FCP/LCP/CLS optimizations</div>
+          <div>FCP/LCP/CLS boost</div>
           <div>Code Splitting</div>
           <div>Tree Shaking</div>
           <div>Critical Rendering Path Optimizations</div>
@@ -50,9 +49,9 @@ function App() {
             <div>Experts in Google Web vitals</div>
             <div>Full-stack experience</div>
             <div>Modern design patterns</div>
-            <div>Ecommerce & Fintech experience</div>
+            <div>Ecommerce & Fintech</div>
             <br/>
-            <div><a href="https://www.krishaweb.com/google-announced-core-web-vitals-new-ranking-factors/">Google’s Core Web Vitals: New Ranking Factors for 2021</a></div>
+            <div><a href="https://www.krishaweb.com/google-announced-core-web-vitals-new-ranking-factors/">Google’s Core Web Vitals: 2021 New Ranking Factors</a></div>
           </div>
           <div className='slide4'>
             <img width="200px" src={amyrisSvg} />
